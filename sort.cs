54f7add2bdefb95eeb43ccb4ee447f6b513ecf83
Jose Manuel Olmedo Martinez <44681304+josemo7@users.noreply.github.com>
2023-08-15 21:50:38 -0600

    Create sort.cs

---
+++ sort.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort2(int n, List<int> arr)
+    {
+        for(int i=1; i< n; i++)
+        {
+            int a = i;
+            for(int j=a-1; j>=0; j--){
+            if(arr[a] < arr[j])
+            {
+                (arr[a],arr[j])=(arr[j],arr[a]);
+                    a=a-1;
+            }
+        }
+        Console.WriteLine(string.Join(' ',arr));
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.insertionSort2(n, arr);
+    }
+}
